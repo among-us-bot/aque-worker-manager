247ce414e77e69189d486188e04e44f39b15dbf8
TAG-Epic <admin@itzepic.net>
2020-11-01 18:29:02 +0100

    Feat: Improve token registring

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ Created by Epic at 11/1/20
 from color_format import basicConfig
 
 from os import environ as env
+from asyncio import Lock
 from aiohttp import WSMessage
 from aiohttp.web import WebSocketResponse, WSMsgType, Application, run_app, get
 from ujson import loads
@@ -14,10 +15,11 @@ basicConfig(logger)
 logger.setLevel(DEBUG)
 
 connected_workers = 0
+connection_lock = Lock()
 guild_workers = {}
 workers = []
 
-tokens = loads(env["WORKER_TOKENS"])
+worker_descriptions = loads(env["WORKER_TOKENS"])
 
 
 async def worker_connection(request):
@@ -25,10 +27,11 @@ async def worker_connection(request):
     ws = WebSocketResponse()
     await ws.prepare(request)
 
+    await connection_lock.acquire()
     msg: WSMessage
     connected_workers += 1
+    connection_num = connected_workers
     worker_info = {}
-
     async for msg in ws:
         if msg.type == WSMsgType.TEXT:
             data = msg.json(loads=loads)
@@ -41,22 +44,24 @@ async def worker_connection(request):
                 guild_workers[event_data] = current_workers
             elif event_name == "identify":
                 worker_info = {
-                    "name": event_data["name"],
+                    "name": worker_descriptions[connection_num]["name"],
+                    "token": worker_descriptions[connection_num]["token"],
                     "ws": ws
                 }
                 workers.append(worker_info)
                 logger.info(f"Worker with name {worker_info['name']} identified, sending token!")
                 await ws.send_json({
-                    "t": "dispatch_token",
-                    "d": tokens[worker_info["name"]]
+                    "t": "dispatch_bot_info",
+                    "d": worker_info
                 })
                 logger.info("Sent token!")
+                connection_lock.release()
             elif event_name == "ratelimit":
                 logger.warning(f"Node {worker_info['name']} got rate-limited. Route: {event_data}")
     connected_workers -= 1
     workers.remove(worker_info)
 
+
 app = Application()
 app.add_routes([get("/workers", worker_connection)])
 run_app(app, host="0.0.0.0", port=6060)
-
