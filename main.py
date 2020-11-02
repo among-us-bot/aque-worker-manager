72fca57b6ff4b4d0fdb78ac71afdd986225c68b1
TAG-Epic <admin@itzepic.net>
2020-11-02 19:10:14 +0100

    Feat: Add controller gateway

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from aiohttp import WSMessage
 from aiohttp.web import WebSocketResponse, WSMsgType, Application, run_app, get
 from ujson import loads
 from logging import getLogger, DEBUG
+from random import choice
 
 logger = getLogger("worker-manager")
 basicConfig(logger)
@@ -44,8 +45,8 @@ async def worker_connection(request):
                 guild_workers[event_data] = current_workers
             elif event_name == "identify":
                 worker_info = {
-                    "name": worker_descriptions[connection_num-1]["name"],
-                    "token": worker_descriptions[connection_num-1]["token"],
+                    "name": worker_descriptions[connection_num - 1]["name"],
+                    "token": worker_descriptions[connection_num - 1]["token"],
                     "ws": ws
                 }
                 workers.append(worker_info)
@@ -65,6 +66,23 @@ async def worker_connection(request):
     workers.remove(worker_info)
 
 
+async def controller_connection(request):
+    ws = WebSocketResponse()
+    await ws.prepare(request)
+
+    msg: WSMessage
+    async for msg in ws:
+        if msg.type == WSMsgType.TEXT:
+            data = msg.json(loads=loads)
+            event_name = data["t"].lower()
+            event_data = data["d"]
+
+            if event_name == "request":
+                guild_id = event_data["guild_id"]
+                available_workers = guild_workers[guild_id]
+                worker = choice(available_workers)
+                await worker["ws"].send_json(event_data["data"])
+
 app = Application()
-app.add_routes([get("/workers", worker_connection)])
+app.add_routes([get("/workers", worker_connection), get("/controller", controller_connection)])
 run_app(app, host="0.0.0.0", port=6060)
