4b3dcbc972a80996aeeedc766a1f16d427158744
TAG-Epic <admin@itzepic.net>
2020-11-28 03:27:32 +0100

    Fix: haha shit fix go brrrrrr

--- analytics.py
+++ analytics.py
@@ -7,7 +7,7 @@ from asyncio import get_event_loop, sleep
 from os import environ as env
 from collections import defaultdict
 
-analytics_url = f"http://{env['DELIVERY_HOST']}:5050/api/:NAME:/:TYPE:"
+analytics_url = f"http://{env['DELIVERY_HOST']}:5050/api/:NAME:/:TYPE:?value=:VALUE:"
 analytic_histogram_cache = defaultdict(lambda: 0)
 
 logger = getLogger("analytics")
@@ -18,8 +18,10 @@ async def track(key: str, val: int, *, analytic_type="gauge"):
         analytic_histogram_cache[key] += 1
         return
     async with ClientSession() as session:
-        await session.post(analytics_url.replace(":NAME:", key).replace(":TYPE:", analytic_type),
-                           headers={"value": val})
+        await session.post(analytics_url
+                           .replace(":NAME:", key)
+                           .replace(":TYPE:", analytic_type)
+                           .replace(":VALUE:", str(val)))
         logger.debug(f"Updating analytics for {key}!")
 
 
@@ -28,8 +30,10 @@ async def histogram_push_loop():
     async with ClientSession() as session:
         while True:
             for name, value in analytic_histogram_cache.items():
-                await session.post(analytics_url.replace(":NAME:", name).replace(":TYPE:", "histogram"),
-                                   headers={"value": value})
+                await session.post(analytics_url
+                                   .replace(":NAME:", name)
+                                   .replace(":TYPE:", "histogram")
+                                   .replace(":VALUE:", str(value)))
             analytic_histogram_cache.clear()
             logger.debug("Updated all histogram analytics!")
             await sleep(30)
